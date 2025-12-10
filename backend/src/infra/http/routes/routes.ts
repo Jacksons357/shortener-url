334790341f3c6599c1078050cff526cc65ce38fc
Jackson Santos <devjackson.contato@gmail.com>
2025-12-09 23:06:32 -0300

    fix: remove routes default

--- backend/src/infra/http/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Application, Router } from 'express'
-import { urlRoutes } from './url.routes'
-
-export function routes(app: Application) {
-	const api = Router()
-
-	api.use('/url', urlRoutes)
-	app.use('/api', api)
-}
